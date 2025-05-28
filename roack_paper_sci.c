54cae45915fda37dc38ceee7865c20d6c940e3a0
Imam Samir Ali <samirali.tech88@gmail.com>
2025-05-28 21:25:29 +0600

    Update roack_paper_sci.c
    formatted the code

--- roack_paper_sci.c
+++ roack_paper_sci.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int win(int player, int guess)//A function whic indicates winning conditions
+int win(int player, int guess) // A function whic indicates winning conditions
 {
-    return
-     (player == 0 && guess == 2) // R>S
-     || (player == 1 && guess == 0) //P>R
-     || (player == 2 && guess == 1);//S>P
+    return (player == 0 && guess == 2)     // R>S
+           || (player == 1 && guess == 0)  // P>R
+           || (player == 2 && guess == 1); // S>P
 }
 int main()
 {
@@ -16,31 +15,33 @@ int main()
     // 1--paper
     // 2--scissors
     int player;
-    printf("---Welcome to Rock Paper Scissors---\n");//small intro
-    printf("There are specified keys for this game \n");//telling user about conditions
+    printf("---Welcome to Rock Paper Scissors---\n");    // small intro
+    printf("There are specified keys for this game \n"); // telling user about conditions
     printf("For Rock Enter = 0\n");
     printf("For Paper Enter = 1\n");
     printf("For Scissor Enter = 2\n");
-    printf("Enter the number : ");//asking for input
-    scanf("%d",&player);//for input
-    printf("Your guess is:%d\n",player);//showing player input
-    printf("Computer guess is:%d\n",guess);//showing computer input
+    printf("Enter the number : ");           // asking for input
+    scanf("%d", &player);                    // for input
+    printf("Your guess is:%d\n", player);    // showing player input
+    printf("Computer guess is:%d\n", guess); // showing computer input
 
-    if(player==guess){
-        printf("----Its a tie----\n");//if both inputs are same
+    if (player == guess)
+    {
+        printf("----Its a tie----\n"); // if both inputs are same
     }
-    else if (win(player,guess))//used win function for win conditon
+    else if (win(player, guess)) // used win function for win conditon
     {
         printf("----You win----\n");
     }
-    else if (player!=0 || player!=1 || player!=2)//if user input any other values
+    else if (player != 0 || player != 1 || player != 2) // if user input any other values
     {
-        printf("Invalid Input\n----You lose----");//He/she will be lose because mistake is mistake
+        printf("Invalid Input\n----You lose----"); // He/she will be lose because mistake is mistake
     }
-    else{
-        printf("----You lose----");//Bad luck
+    else
+    {
+        printf("----You lose----"); // Bad luck
     }
-    
 
     return 0;
 }
+
